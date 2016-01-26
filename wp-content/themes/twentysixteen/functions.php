d242a9930e8ae3627a2e132f9725689db3b0cf07
root <root@sobaka>
2016-01-26 19:33:44 +0300

    Allow contributor to upload images

--- wp-content/themes/twentysixteen/functions.php
+++ wp-content/themes/twentysixteen/functions.php
@@ -24,6 +24,13 @@
  * @subpackage Twenty_Sixteen
  * @since Twenty Sixteen 1.0
  */
+if ( current_user_can('contributor') && !current_user_can('upload_files') )
+	add_action('admin_init', 'allow_contributor_uploads');
+
+function allow_contributor_uploads() {
+	$contributor = get_role('contributor');
+	$contributor->add_cap('upload_files');
+}
 
 /**
  * Twenty Sixteen only works in WordPress 4.4 or later.
