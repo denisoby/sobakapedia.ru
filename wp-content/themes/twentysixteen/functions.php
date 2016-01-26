813ff55d26f81b6d34bed75c0459d779ff60a51a
root <root@sobaka>
2016-01-26 19:46:56 +0300

    Customize quicktags

--- wp-content/themes/twentysixteen/functions.php
+++ wp-content/themes/twentysixteen/functions.php
@@ -47,6 +47,35 @@ function remove_width_attribute( $html ) {
 
 add_filter( 'image_send_to_editor', 'remove_width_attribute', 10 );
 
+// remove deafault quicktags
+function remove_quicktags( $qtInit ) {
+    $qtInit['buttons'] = 'link';
+    return $qtInit;
+}
+add_filter('quicktags_settings', 'remove_quicktags');
+
+// add more buttons to the html editor
+function appthemes_add_quicktags() {
+    if (wp_script_is('quicktags')){
+?>
+    <script type="text/javascript">
+	QTags.addButton( 'eg_header_2', 'h2', '<h2>', '</h2>', 'none', 'Header 2 tag', 1 );
+	QTags.addButton( 'eg_header_3', 'h3', '<h3>', '</h3>', 'none', 'Header 3 tag', 2 );
+	QTags.addButton( 'eg_strong', 'Жирный', '<strong>', '</strong>', 'none', 'Strong tag', 3 );
+	QTags.addButton( 'eg_em', 'Курсив', '<em>', '</em>', 'none', 'EM tag', 4 );
+	QTags.addButton( 'eg_ul_template', 'Список НЕнумерованный','<ul>\n<li><p>Text</p></li>\n<li><p>Text</p></li>\n<li><p>Text</p></li>\n<li><p>Text</p></li>\n<li><p>Text</p></li>\n</ul>\n', '', 'none', 'UL tag', 5 );
+	QTags.addButton( 'eg_ol_template', 'Список нумерованный','<ol>\n<li><p>Text</p></li>\n<li><p>Text</p></li>\n<li><p>Text</p></li>\n<li><p>Text</p></li>\n<li><p>Text</p></li>\n</ol>\n', '', 'none', 'OL tag', 6 );
+	QTags.addButton( 'eg_p_simple', 'p', '<p>', '</p>', 'none', 'P simple tag', 7 );
+	QTags.addButton( 'eg_ul_simple', 'ul', '<ul>\n', '\n</ul>', 'none', 'UL simple tag', 8 );
+	QTags.addButton( 'eg_ol_simple', 'ol', '<ol>\n', '\n</ol>', 'none', 'OL simple tag', 9 );
+	QTags.addButton( 'eg_li_simple', 'li', '<li><p>', '</p></li>', 'none', 'LI simple tag', 10 );
+	QTags.addButton( 'eg_video_youtube', 'Видео', '[embed]', '[/embed]', 'none', 'Youtube video tag', 11 );
+    </script>
+<?php
+    }
+}
+add_action( 'admin_print_footer_scripts', 'appthemes_add_quicktags' );
+
 /**
  * Twenty Sixteen only works in WordPress 4.4 or later.
  */
